f7134389d572ee0e8d0760821f96cffc675fadd8
yonas tatek <yonastatekcse1@gmail.com>
2022-01-31 07:56:41 +0300

    ji

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,16 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+    
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory:"./migrations"
     }
   },
 
